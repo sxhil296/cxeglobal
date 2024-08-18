8be31047bafb3747332dd2755a9053c08a886722
Sahil <leviack629@gmail.com>
2024-08-18 18:56:16 +0530

    top navbar

---
+++ src/app/components/TopNavbar.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaInstagram,
+  FaLinkedinIn,
+} from "react-icons/fa";
+
+const TopNavbar = () => {
+  return (
+    <div className="w-full bg-blue-400 py-2 top-0 fixed">
+      <div className="max-w-[1280px] mx-auto flex justify-between items-center px-4 lg:px-8">
+        {/* Moving Text */}
+        <div className="hidden lg:block overflow-hidden whitespace-nowrap">
+          <div className="animate-marquee text-white font-medium">
+            Welcome to CXE-Global - Transforming Your Business with Innovation!
+          </div>
+        </div>
+
+        {/* Social Icons */}
+        <div className="flex justify-center w-full lg:w-auto lg:justify-end">
+          <div className="flex space-x-4 text-white">
+            <a
+              href="https://facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaFacebookF />
+            </a>
+            <a
+              href="https://twitter.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaTwitter />
+            </a>
+            <a
+              href="https://instagram.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaInstagram />
+            </a>
+            <a
+              href="https://linkedin.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedinIn />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TopNavbar;
